c1807569b4bde1530748b23180c9d5cff4cf6c9f
Ray C Horn <raychorn@gmail.com>
2021-03-20 20:22:18 -0500

    updates.

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 version=3.9
+VENV=.venv
+REQS=./requirements.txt
 
 py1=$(whereis python | grep $version)
 
@@ -29,4 +31,35 @@ then
     echo ""
 else
     echo "Python v$version has been installed."
+    py39=$(which python3.9)
+    echo "py39 -> $py39"
+    $sudoprefix apt-get install python3-pip -y
+    pypip3=$(which pip3)
+    echo "pypip3 -> $pypip3"
+    $pypip3 --version
+    $pypip3 install virtualenv
+    $pypip3 list
+    v=$($py39 -c 'import sys; i=sys.version_info; print("{}{}{}".format(i.major,i.minor,i.micro))')
+    echo "Use this -> $py39 --> $v"
+    VENV=$VENV$v
+    echo "VENV -> $VENV"
+    if [[ -d $VENV ]]
+    then
+        rm -R -f $VENV
+    fi
+    if [[ ! -d $VENV ]]
+    then
+        virtualenv --python $py39 -v $VENV
+    fi
+    if [[ -d $VENV ]]
+    then
+        . ./$VENV/bin/activate
+        pip install --upgrade pip
+
+        if [[ -f $REQS ]]
+        then
+            pip install -r $REQS
+        fi
+
+    fi
 fi
